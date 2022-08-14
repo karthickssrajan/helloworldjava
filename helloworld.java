07819381f4e3effefc24f4150f99cc676ed44cde
karthickssrajan <109479490+karthickssrajan@users.noreply.github.com>
2022-08-14 22:24:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - at 03:35 pm");
+    System.out.println("Hello World - at 10:25 pm");
     }
 }
