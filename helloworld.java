119b2b3a134f41a20b97e1cf665799513ed3665c
karthickssrajan <109479490+karthickssrajan@users.noreply.github.com>
2022-08-14 22:46:47 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - at 10:25 pm");
+    System.out.println("Hello World - at 10:46 pm - triggered by webhook");
     }
 }
